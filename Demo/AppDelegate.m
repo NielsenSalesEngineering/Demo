19724b2e5ac7b92bda41994851567a241597d188
Wes Kincaid <wesley.kincaid@nielsen.com>
2015-06-15 12:29:41 -0400

    Corrected date in header.

--- Demo/AppDelegate.m
+++ Demo/AppDelegate.m
@@ -2,7 +2,7 @@
 //  AppDelegate.m
 //  Demo
 //
-//  Created by Wesley Kincaid on 5/26/15.
+//  Created by Wesley Kincaid on 4/26/15.
 //  Copyright (c) 2015 Nielsen. All rights reserved.
 //
 
--- Demo/main.m
+++ Demo/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  Demo
 //
-//  Created by Wesley Kincaid on 5/26/15.
+//  Created by Wesley Kincaid on 4/26/15.
 //  Copyright (c) 2015 Nielsen. All rights reserved.
 //
 
