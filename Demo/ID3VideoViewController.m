5aa887d5aee68f20f37a086c296d8e014e31b575
Wes Kincaid <wesley.kincaid@nielsen.com>
2015-06-15 11:32:08 -0400

    Configure key observer pattern.

--- Demo/ID3VideoViewController.m
+++ Demo/ID3VideoViewController.m
@@ -6,28 +6,31 @@
 //  Copyright (c) 2015 Nielsen. All rights reserved.
 //
 
+#import "ID3VideoViewController.h"
+
 #import <CoreMedia/CoreMedia.h>
 #import <AVFoundation/AVFoundation.h>
 #import "NielsenAppApi/NielsenAppApi.h"
-#import "ID3VideoViewController.h"
-#import "PlayerView.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AVKit/AVKit.h>
 
-//static void *MyStreamingMovieViewControllerTimedMetadataObserverContext = &MyStreamingMovieViewControllerTimedMetadataObserverContext;
-//static void *MyStreamingMovieViewControllerRateObservationContext = &MyStreamingMovieViewControllerRateObservationContext;
-//static void *MyStreamingMovieViewControllerCurrentItemObservationContext = &MyStreamingMovieViewControllerCurrentItemObservationContext;
-//static void *MyStreamingMovieViewControllerPlayerItemStatusObserverContext = &MyStreamingMovieViewControllerPlayerItemStatusObserverContext;
 
 NielsenAppApi *nielsenMeter = nil;
 NSArray* array;
-NSTimer *playheadTimer;
+NSString *playerInfo;
+
 
 @interface ID3VideoViewController ()
 
+@property (nonatomic, retain) AVPlayerViewController *avPlayerViewcontroller;
+
 @end
+
 
 @implementation ID3VideoViewController
 
 @synthesize player, playerView, playerItem;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -43,9 +46,6 @@ NSTimer *playheadTimer;
     NSString *jsonStringNielsenConfig = [[NSString alloc] initWithBytes:[jsonDataNielsenConfig bytes] length:[jsonDataNielsenConfig length] encoding:NSUTF8StringEncoding];
     nielsenMeter = [[NielsenAppApi alloc] initWithAppInfo:jsonStringNielsenConfig delegate:self];
     
-    // Configure a play head timer
-    //playheadTimer = [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(playHeadTimeEvent) userInfo:nil repeats:YES];
-    
     // Configure the player
     NSDictionary *playerInfoDict = @{
                                  @"channelName": @"Video Demo",
@@ -53,13 +53,13 @@ NSTimer *playheadTimer;
                                  @"dataSrc": @"id3"
                                  };
     NSData *playerInfoData = [NSJSONSerialization dataWithJSONObject:playerInfoDict options:0 error:nil];
-    NSString *playerInfo = [[NSString alloc] initWithBytes:[playerInfoData bytes] length:[playerInfoData length] encoding:NSUTF8StringEncoding];
-    [nielsenMeter play:playerInfo];
+    playerInfo = [[NSString alloc] initWithBytes:[playerInfoData bytes] length:[playerInfoData length] encoding:NSUTF8StringEncoding];
     
     // Configure the asset
+    NSURL *url = [NSURL URLWithString:@"http://nielsense-assets.s3.amazonaws.com/id3/001/prog_index.m3u8"];
     NSDictionary *assetInfoDict = @{
                                 @"type": @"content",
-                                @"assetid": @"assetid",
+                                @"assetid": @"",
                                 @"tv": @"true",
                                 @"program": @"MyProgram",
                                 @"title": @"MyEpisodeTitle",
@@ -70,93 +70,112 @@ NSTimer *playheadTimer;
     NSData *assetInfoData = [NSJSONSerialization dataWithJSONObject:assetInfoDict options:0 error:nil];
     NSString *assetInfo = [[NSString alloc] initWithBytes:[assetInfoData bytes] length:[assetInfoData length] encoding:NSUTF8StringEncoding];
     [nielsenMeter loadMetadata:assetInfo];
+
+    // Configure an AVPlayerViewController
+    AVPlayerViewController *playerViewController = [[AVPlayerViewController alloc] init];
+    playerViewController.player = [AVPlayer playerWithURL:url];
+    self.avPlayerViewcontroller = playerViewController;
+    [self resizePlayerToViewSize];
+    [self.view addSubview:playerViewController.view];
+    self.view.autoresizesSubviews = TRUE;
     
-    NSURL *url = [NSURL URLWithString:@"http://nielsense-assets.s3.amazonaws.com/id3/001/prog_index.m3u8"];
-    player = [AVPlayer playerWithURL:url];
+    // Register Key Value Observers on status and rate attributes
+    [self.avPlayerViewcontroller.player addObserver:self forKeyPath:@"status" options:0 context:nil];
+    [self.avPlayerViewcontroller.player addObserver:self forKeyPath:@"rate" options:0 context:nil];
     
-    [self.playerView setPlayer:player];
-    [player play];
+    // Observe player every 2 seconds and update playheadPosition
+    CMTime i = CMTimeMakeWithSeconds(2.0, NSEC_PER_SEC);
+    [self.avPlayerViewcontroller.player addPeriodicTimeObserverForInterval:i queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+        NSLog(@"Update playhead position");
+        CMTime t = [self.avPlayerViewcontroller.player currentTime];
+        long position = CMTimeGetSeconds(t);
+        [nielsenMeter playheadPosition:position];
+    }];
 }
 
-//-(void)playHeadTimeEvent {
-//    CMTime currentTime = [player currentTime];
-//    int position = CMTimeGetSeconds(currentTime);
-//    [nielsenMeter playheadPosition:position];
-//}
-//
-//-(void)handleTimedMetada:(AVMetadataItem*)timedMetadata {
-//    id extraAttributeType = [timedMetadata extraAttributes];
-//    NSString *extraString = nil;
-//    if ([extraAttributeType isKindOfClass:[NSDictionary class]]) {
-//        extraString = [extraAttributeType valueForKey:@"info"];
-//    }
-//    else if ([extraAttributeType isKindOfClass:[NSString class]]) {
-//        extraString = extraAttributeType;
-//    }
-//    
-//    if ([(NSString *)[timedMetadata key] isEqualToString:@"PRIV"] && [extraString rangeOfString:@"www.nielsen.com"].length > 0) {
-//        if ([[timedMetadata value] isKindOfClass:[NSData class]]) {
-//            // Send ID3 Tag
-//            [nielsenMeter sendID3:extraString];
-//            NSString *value = [timedMetadata stringValue];
-//            if (value != nil) {
-//                NSLog(extraString);
-//            }
-//        }
-//    } else {
-//        NSLog(@"Could not send ID3 Tags");
-//    }
-//};
+- (void) resizePlayerToViewSize {
+    CGRect frame = self.view.frame;
+    NSLog(@"frame size %d, %d", (int)frame.size.width, (int)frame.size.height);
+    self.avPlayerViewcontroller.view.frame = frame;
+}
 
+-(void)handleTimedMetada:(AVMetadataItem*)timedMetadata {
+    NSLog(@"handleTimedMetadata");
+    id extraAttributeType = [timedMetadata extraAttributes];
+    NSString *extraString = nil;
+    if ([extraAttributeType isKindOfClass:[NSDictionary class]]) {
+        extraString = [extraAttributeType valueForKey:@"info"];
+    }
+    else if ([extraAttributeType isKindOfClass:[NSString class]]) {
+        extraString = extraAttributeType;
+    }
+    
+    if ([(NSString *)[timedMetadata key] isEqualToString:@"PRIV"] && [extraString rangeOfString:@"www.nielsen.com"].length > 0) {
+        if ([[timedMetadata value] isKindOfClass:[NSData class]]) {
+            // Send ID3 Tag
+            [nielsenMeter sendID3:extraString];
+            NSString *value = [timedMetadata stringValue];
+            if (value != nil) {
+                NSLog(extraString);
+            }
+        }
+    } else {
+        NSLog(@"Could not send ID3 Tags");
+    }
+};
 
-//- (void)observeValueForKeyPath:(NSString *)path ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-//    NSLog(@"Observed value");
-//    @try {
-//        if (context == MyStreamingMovieViewControllerPlayerItemStatusObserverContext) {
-//            AVPlayerStatus status = [[change objectForKey:NSKeyValueChangeNewKey] integerValue];
-//            switch (status) {
-//                case AVPlayerStatusReadyToPlay:
-//                    break;
-//                case AVPlayerStatusFailed:
-//                    break;
-//                case AVPlayerStatusUnknown:
-//                    break;
-//            }
-//        } else if (context == MyStreamingMovieViewControllerCurrentItemObservationContext) {
-//            NSLog(@"MyStreamingMovieViewControllerCurrentItemObservationContext");
-//        } else if (context == MyStreamingMovieViewControllerTimedMetadataObserverContext) {
-//            array = [[player currentItem] timedMetadata];
-//            for (AVMetadataItem *metadataItem in array) {
-//                [self handleTimedMetada:metadataItem];
-//            }
-//        } else {
-//            if (context && object) {
-//                [self observeValueForKeyPath:path ofObject:object change:change context:context];
-//            }
+- (void)observeValueForKeyPath:(NSString *)path ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+
+    // Watch for rate changes to handle pauses in playback.  A rate of 0 is paused.
+    if ([path isEqualToString:@"rate"]) {
+        if ([self.avPlayerViewcontroller.player rate]) {
+            NSLog(@"Unpaused.");
+            [nielsenMeter play:playerInfo];
+        } else {
+            NSLog(@"Paused.");
+            [nielsenMeter stop];
+        }
+    
+    // Watch status to play asset once loaded
+    } else if ([path isEqualToString:@"status"]) {
+        if (self.avPlayerViewcontroller.player.status == AVPlayerItemStatusReadyToPlay) {
+            NSLog(@"Ready to play");
+            [self.avPlayerViewcontroller.player play];
+            [nielsenMeter play:playerInfo];
+        }
+    }
+
+//    if (context == MyStreamingMovieViewControllerCurrentItemObservationContext) {
+//        NSLog(@"MyStreamingMovieViewControllerCurrentItemObservationContext");
+//    } else if (context == MyStreamingMovieViewControllerTimedMetadataObserverContext) {
+//        NSLog(@"MyStreamingMovieViewControllerTimedMetadataObserverContext");
+//        array = [[player currentItem] timedMetadata];
+//        for (AVMetadataItem *metadataItem in array) {
+//            [self handleTimedMetada:metadataItem];
 //        }
 //    }
-//    @catch (NSException *exception) {
-//        NSLog(@"catching %@ reason %@", [exception name], [exception reason]);
-//    }
 //}
-//
-//-(void)notifyInActive:(NSNotification *)notification {
-//    NSLog(@"Sample player is Notified by a Event : %@",notification.userInfo);
-//}
-//
-//- (void)didReceiveMemoryWarning {
-//    [super didReceiveMemoryWarning];
-//}
+}
+
+-(void)notifyInActive:(NSNotification *)notification {
+    NSLog(@"notifyInActive: %@", notification.userInfo);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
 
 
 # pragma mark -
 # pragma mark Nielsen App API Delegates
 # pragma mark -
 
+// Fulfill Nielsen App API Delegate
 - (void)nielsenAppApi:(NielsenAppApi *)appApi eventOccurred:(NSDictionary *)event {
     NSLog(@"Sample player is Notified by a Event : %@", event);
 }
 
+// Fulfill Nielsen App API Delegate
 - (void)nielsenAppApi:(NielsenAppApi *)appApi errorOccurred:(NSDictionary *)error {
     NSLog(@"Sample player is Notified by an Error : %@", error);
 }
